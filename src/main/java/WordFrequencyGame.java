450374bdfd9ec56a9bb8c91d86c5a600135ded3e
cody-chen-jf <459741515@qq.com>
2018-05-30 20:27:01 +0800

    split the method 4

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,30 +3,35 @@ import java.util.stream.Collectors;
 
 class WordFrequencyGame {
 
+    public static final String SPACE_REGEX = "\\s+";
+    public static final String DELIMITER = "\n";
+
     String play(String inputStr) {
         List<String> buildTexts = getBuildTexts(inputStr);
-        List<Word> wordList = getWords(getStringListMap(inputStr));
+        List<Word> wordList = getWords(buildTexts);
         return getOutputString(wordList);
     }
 
     private List<String> getBuildTexts(String inputStr) {
-        return Arrays.asList(inputStr.split("\\s+"));
+        return Arrays.asList(inputStr.split(SPACE_REGEX));
     }
 
-    private List<Word> getWords(Map<String, List<Word>> map) {
-        return map.entrySet().stream()
+    private List<Word> getWords(List<String> texts) {
+        return texts.stream()
+                .map(s -> new Word(s, 1))
+                .collect(Collectors.toList())
+                .stream()
+                .collect(Collectors.groupingBy(Word::getText))
+                .entrySet()
+                .stream()
                 .map(entry -> new Word(entry.getKey(), entry.getValue().size()))
                 .collect(Collectors.toList());
-    }
-
-    private Map<String, List<Word>> getStringListMap(String inputStr) {
-        return Arrays.stream(inputStr.split("\\s+")).map(s -> new Word(s, 1)).collect(Collectors.toList()).stream().collect(Collectors.groupingBy(Word::getText));
     }
 
     private String getOutputString(List<Word> wordList) {
         return wordList.stream()
                 .sorted((w1, w2) -> w2.getCount() - w1.getCount())
                 .map(word -> word.getText() + " " + word.getCount())
-                .collect(Collectors.joining("\n"));
+                .collect(Collectors.joining(DELIMITER));
     }
 }
