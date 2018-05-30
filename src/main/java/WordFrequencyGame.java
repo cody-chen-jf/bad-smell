439a18a234ee246a139ce15c9568223dd82a0db7
cody-chen-jf <459741515@qq.com>
2018-05-30 20:06:39 +0800

    split the method 2

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,22 +9,19 @@ public class WordFrequencyGame {
     }
 
     private String getOutputString(List<Word> wordList) {
-        wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
-
-        StringJoiner joiner = new StringJoiner("\n");
-        for (Word w : wordList) {
-            String s = w.getText() + " " + w.getCount();
-            joiner.add(s);
-        }
-        return joiner.toString();
+        return wordList.stream()
+                .sorted((w1, w2) -> w2.getCount() - w1.getCount())
+                .map(word -> word.getText() + " " + word.getCount())
+                .collect(Collectors.joining("\n"));
     }
 
     private List<Word> getWords(Map<String, List<Word>> map) {
-        return map.entrySet().stream().map(entry -> new Word(entry.getKey(), entry.getValue().size())).collect(Collectors.toList());
+        return map.entrySet().stream()
+                .map(entry -> new Word(entry.getKey(), entry.getValue().size()))
+                .collect(Collectors.toList());
     }
 
     private Map<String, List<Word>> getStringListMap(String inputStr) {
-        //get the map for the next step of sizing the same word
         return Arrays.stream(inputStr.split("\\s+")).map(s -> new Word(s, 1)).collect(Collectors.toList()).stream().collect(Collectors.groupingBy(Word::getText));
     }
 
