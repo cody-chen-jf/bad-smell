432ae25c83f05b1a4dd5e65aa030852b001402bf
cody-chen-jf <459741515@qq.com>
2018-05-30 17:49:09 +0800

    extract method 2

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,55 +1,38 @@
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-public class WordFrequencyGame {
-    public String getResult(String inputStr) {
+class WordFrequencyGame {
+    String play(String inputStr) {
         Map<String, List<Word>> map = getStringListMap(inputStr);
         List<Word> wordList = getWords(map);
         return getWordList(wordList);
     }
 
-    public String getWordList(List<Word> wordList) {
-        wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
-
-        StringJoiner joiner = new StringJoiner("\n");
-        for (Word w : wordList) {
-            String s = w.getText() + " " + w.getCount();
-            joiner.add(s);
-        }
-        return joiner.toString();
+    private String getWordList(List<Word> wordList) {
+//        wordList;
+//        StringJoiner joiner = new StringJoiner("\n");
+//
+//        for (Word w : wordList) {
+//            String s = w.getText() + " " + w.getCount();
+//            joiner.add(s);
+//        }
+        return wordList.stream().sorted((w1, w2) -> w2.getCount() - w1.getCount()).map(w -> w.getText() + " " + w.getCount()).collect(Collectors.joining("\n"));
     }
 
-    public List<Word> getWords(Map<String, List<Word>> map) {
-        List<Word> list = new ArrayList<>();
-        for (Map.Entry<String, List<Word>> entry : map.entrySet()) {
-            Word word = new Word(entry.getKey(), entry.getValue().size());
-            list.add(word);
-        }
-        return list;
+    private List<Word> getWords(Map<String, List<Word>> map) {
+        return map.entrySet().stream().map(entry -> new Word(entry.getKey(), entry.getValue().size())).collect(Collectors.toList());
     }
 
-    public Map<String, List<Word>> getStringListMap(String inputStr) {
-        String[] arr = inputStr.split("\\s+");
-
-        List<Word> wordList = new ArrayList<>();
-        for (String s : arr) {
-            Word word = new Word(s, 1);
-            wordList.add(word);
-        }
-
-        return getListMap(wordList);
+    private Map<String, List<Word>> getStringListMap(String inputStr) {
+        //            if (map.containsKey(word.getText())) {
+//                map.get(word.getText()).add(word);
+//            } else {
+//                ArrayList arr = new ArrayList<>();
+//                arr.add(word);
+//                map.put(word.getText(), arr);
+//            }
+        return Arrays.stream(inputStr.split("\\s+")).map(s -> new Word(s, 1)).collect(Collectors.toList()).stream().collect(Collectors.groupingBy(Word::getText));
     }
 
-    private Map<String, List<Word>> getListMap(List<Word> wordList) {
-        Map<String, List<Word>> map = new HashMap<>();
-        for (Word word : wordList) {
-            if (!map.containsKey(word.getText())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(word);
-                map.put(word.getText(), arr);
-            } else {
-                map.get(word.getText()).add(word);
-            }
-        }
-        return map;
-    }
 }
