c5dd25e8919b974200e513a25cebc069b54d77f9
cody-chen-jf <459741515@qq.com>
2018-05-30 19:51:52 +0800

    remove try catch

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,36 +3,24 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public String play(String inputStr) {
+        Map<String, List<Word>> map = getStringListMap(inputStr);
+        List<Word> wordList;
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
-        } else {
-
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                Map<String, List<Word>> map = getStringListMap(inputStr);
-                List<Word> wordList;
-
-                List<Word> list = new ArrayList<>();
-                for (Map.Entry<String, List<Word>> entry : map.entrySet()) {
-                    Word word = new Word(entry.getKey(), entry.getValue().size());
-                    list.add(word);
-                }
-                wordList = list;
-
-                wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Word w : wordList) {
-                    String s = w.getText() + " " + w.getCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
+        List<Word> list = new ArrayList<>();
+        for (Map.Entry<String, List<Word>> entry : map.entrySet()) {
+            Word word = new Word(entry.getKey(), entry.getValue().size());
+            list.add(word);
         }
+        wordList = list;
+
+        wordList.sort((w1, w2) -> w2.getCount() - w1.getCount());
+
+        StringJoiner joiner = new StringJoiner("\n");
+        for (Word w : wordList) {
+            String s = w.getText() + " " + w.getCount();
+            joiner.add(s);
+        }
+        return joiner.toString();
     }
 
     private Map<String, List<Word>> getStringListMap(String inputStr) {
